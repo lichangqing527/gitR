d60f369b95c71c4a19ec808da92c2e1eee0f8eb9
lichangqing527 <1598080683@qq.com>
2021-11-11 20:57:55 +0800

    update

--- temperAutoClock.py
+++ temperAutoClock.py
@@ -1,32 +1,62 @@
 # -*- coding: UTF-8 -*-
-#cron: 30 7 * * *
-#项目名称: gitR/temperAutoClock123
+# cron: 30 7 * * *
+# 项目名称: gitR/temperAutoClock123
 import json
 import random
 import time
 import requests
 import sys
 import os
+
 cur_path = os.path.abspath(os.path.dirname(__file__))
 root_path = os.path.split(cur_path)[0]
 sys.path.append(root_path)
 
-#初始化变量
-token=''#pushplustoken
+# 初始化变量
+token = ''  # pushplustoken
 Authorization = ''
 
-#变量赋值
+# 变量赋值
 if "WX_KEY" in os.environ and os.environ["WX_KEY"]:
     Authorization = 'Bearer ' + os.environ["WX_KEY"]
 if "PUSH_PLUS_TOKEN" in os.environ and os.environ["PUSH_PLUS_TOKEN"]:
     token = os.environ["PUSH_PLUS_TOKEN"]
 
 logs = ""
+
+url = 'https://xsgzgl.zxhnzq.com/api/EducationAPI/GetDqXqInfo?isrefresh=true&scode=10410&sccode=1041001'
+headers = {'Host': 'xsgzgl.zxhnzq.com', 'Connection': 'keep-alive',
+           'Authorization': Authorization,
+           'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36 MicroMessenger/7.0.9.501 NetType/WIFI MiniProgramEnv/Windows WindowsWechat',
+           'content-type': 'application/json', 'env': 'production', 'positionID': '', 'terminal': 'miniprogram',
+           'version': '2.0.44', 'Referer': 'https://servicewechat.com/wx91da385bdd520809/145/page-frame.html',
+           'Accept-Encoding': 'gzip, deflate, br'}
+html = requests.get(url, headers=headers)
+html = json.loads(html.text)
+xh = html.get("data").get("XueQi")
+
+biztime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+url = 'https://xsgzgl.zxhnzq.com/api/PositionInfo/AddOrEditPosition'
+data = {"usercode": "20191799", "xq": xh, "typeid": 5, "remark": "体温打卡", "biztime": biztime, "lat": 28.68202,
+        "lng": 115.85794, "address": "江西省南昌市红谷滩区世贸路144号(红谷滩区南昌市人民政府(世贸路))", "province": "江西省", "city": "南昌市",
+        "district": "红谷滩区", "street": "世贸路", "scode": "10410", "sccode": "1041001"}
+data = json.dumps(data)
+html = requests.post(url, headers=headers, data=data)  # 发送获取位置id的请求
+html = json.loads(html.text)
+positionID = html.get("data")  # positionID
+if html.get("code") == 200:
+    logs = logs + '位置获取 √ \n'
+else:
+    logs = logs + '位置获取 × ' + html.get("msg") + '\n'
+    print(logs)
+    exit(0)
+
 url = 'https://xsgzgl.zxhnzq.com/api/ScancodeRegister/GetMyScanCode?scode=10410&sccode=1041001'
 headers = {'Host': 'xsgzgl.zxhnzq.com', 'Connection': 'keep-alive',
            'Authorization': Authorization,
            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36 MicroMessenger/7.0.9.501 NetType/WIFI MiniProgramEnv/Windows WindowsWechat',
-           'content-type': 'application/json', 'env': 'production', 'positionID': '4955747', 'terminal': 'miniprogram',
+           'content-type': 'application/json', 'env': 'production', 'positionID': str(positionID),
+           'terminal': 'miniprogram',
            'version': '2.0.44', 'Referer': 'https://servicewechat.com/wx91da385bdd520809/145/page-frame.html',
            'Accept-Encoding': 'gzip, deflate, br'}
 
@@ -40,29 +70,6 @@ else:
     print(logs)
     exit(0)
 
-biztime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-url = 'https://xsgzgl.zxhnzq.com/api/PositionInfo/AddOrEditPosition'
-data = {"usercode": "20191799", "xq": "20211", "typeid": 5, "remark": "体温打卡", "biztime": biztime, "lat": 28.68202,
-        "lng": 115.85794, "address": "江西省南昌市红谷滩区世贸路144号(红谷滩区南昌市人民政府(世贸路))", "province": "江西省", "city": "南昌市",
-        "district": "红谷滩区", "street": "世贸路", "scode": "10410", "sccode": "1041001"}
-data = json.dumps(data)
-data_len = len(data)
-headers = {'Host': 'xsgzgl.zxhnzq.com', 'Connection': 'keep-alive', 'Authorization': Authorization,
-           'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36 MicroMessenger/7.0.9.501 NetType/WIFI MiniProgramEnv/Windows WindowsWechat',
-           'content-type': 'application/json', 'env': 'production', 'positionID': '', 'terminal': 'miniprogram',
-           'version': '2.0.44', 'Referer': 'https://servicewechat.com/wx91da385bdd520809/145/page-frame.html',
-           'Accept-Encoding': 'gzip, deflate, br'}
-
-html = requests.post(url, headers=headers, data=data)  # 发送获取位置id的请求
-html = json.loads(html.text)
-positionID = html.get("data")  # positionID
-if html.get("code") == 200:
-    logs = logs + '位置获取 √ \n'
-else:
-    logs = logs + '位置获取 × ' + html.get("msg") + '\n'
-    print(logs)
-    exit(0)
-
 # 时间格式转换
 registerdate = time.time() - 60 * 60 * 8
 registerdate = time.strftime(
@@ -70,12 +77,7 @@ registerdate = time.strftime(
     time.localtime(registerdate))
 
 url = 'https://xsgzgl.zxhnzq.com/api/BatchSignin/Add'
-headers = {'Host': 'xsgzgl.zxhnzq.com', 'Connection': 'keep-alive', 'Authorization': Authorization, 'charset': 'utf-8',
-           'positionid': str(positionID),
-           'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36 MicroMessenger/7.0.9.501 NetType/WIFI MiniProgramEnv/Windows WindowsWechat',
-           'content-type': 'application/json', 'terminal': 'miniprogram', 'Accept-Encoding': 'gzip,compress,br,deflate',
-           'env': 'production', 'version': '2.0.44',
-           'Referer': 'https://servicewechat.com/wx91da385bdd520809/145/page-frame.html'}
+
 bodytemperature = str(round(random.uniform(36.3, 37), 1))  # 随机生成体温
 data = {"batchid": 10, "positionid": positionID, "verifystate": 0,
         "health_Student": {"xh": xh, "registerdate": registerdate, "bodytemperature": bodytemperature,
@@ -107,4 +109,10 @@ print(logs)
 if '发送消息成功' in r:
     print("推送成功")
 else:
-    print("推送失败")
+    r = requests.post('https://www.pushplus.plus/send', data).text
+    if '请求成功' in r:
+        print("推送成功")
+    else:
+        print("推送失败")
+
+
